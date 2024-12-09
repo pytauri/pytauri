a689836595d3edeba8eb1310c95c865c8db0f1a1
Bjorn <75190918+BjornTheProgrammer@users.noreply.github.com>
2024-12-08 23:41:10 -0700

    Implemented `pytauri-bundler` command

--- crates/pytauri-bundler/src/main.rs
+++ crates/pytauri-bundler/src/main.rs
@@ -1,36 +1,54 @@
-#![allow(dead_code)]
+use std::{collections::HashMap, fs, process::Command};
+use anyhow::Result;
+use serde::Serialize;
+use pytauri_bundler::download_standalone;
+use std::env;
 
-
-use pytauri_bundler::{get_cargo_toml_path, download_standalone};
-use std::{env, fs::create_dir_all, error::Error};
-
-fn main() -> Result<(), Box<dyn Error>> {
-    let debug_mode = match env::var("TAURI_ENV_DEBUG") {
-        Ok(val) if val == "true" => true,
-        _ => false
-    };
+fn main() -> Result<()> {
+    let out_dir = env::temp_dir();
 
     let python_version = match env::var("PYTAURI_PYTHON_VERSION") {
         Ok(version) => version,
         _ => "3.10.16".to_string()
     };
 
-    let target_triple = env::var("TAURI_ENV_TARGET_TRIPLE")?;
-    let _arch = env::var("TAURI_ENV_ARCH")?;
-    let platform = env::var("TAURI_ENV_PLATFORM")?;
+    let target_triple = env!("TARGET");
 
-    if debug_mode == true {
-        panic!("Cannot bundle in debug mode")
+    let mut python_out_path = out_dir.clone();
+    python_out_path.push("pyembed");
+
+    download_standalone(&python_version, &target_triple, python_out_path.clone())?;
+
+    let location = python_out_path.canonicalize().unwrap().display().to_string();
+
+    #[derive(Serialize)]
+    struct BundleConfig {
+       active: bool,
+       targets: String,
+       resources: HashMap<String, String>
     }
 
-    if platform == "windows" || platform == "darwin" {
-        let mut output_dir = get_cargo_toml_path()?;
-        output_dir.push("resources");
-        create_dir_all(&output_dir)?;
-        output_dir.push(format!("cpython-{}", target_triple));
-
-        download_standalone(&python_version, &target_triple, output_dir)?;
+    #[derive(Serialize)]
+    struct Config {
+       bundle: BundleConfig
     }
+
+    let mut config = Config {
+        bundle: BundleConfig {
+            active: true,
+            targets: "all".to_string(),
+            resources: HashMap::new()
+        }
+    };
+
+    config.bundle.resources.insert(location, "pyembed".to_string());
+    config.bundle.resources.insert("python".into(), "python".into());
+
+    let json_config = serde_json::to_string(&config)?;
+
+    Command::new("cargo-tauri")
+        .args(["build", "--config", &json_config])
+        .status()?;
 
     Ok(())
 }
--- example/build.rs
+++ example/build.rs
@@ -1,55 +1,7 @@
-use std::{collections::HashMap, env, path::PathBuf};
-
 use anyhow::Result;
-use pytauri_bundler::download_standalone;
-use serde::Serialize;
-use serde_json::json;
-
-
-macro_rules! p {
-    ($($tokens: tt)*) => {
-        println!("cargo:warning={}", format!($($tokens)*))
-    }
-}
-
 
 fn main() -> Result<()> {
-    let out_dir = PathBuf::from(env::var("OUT_DIR")?);
-
-    let python_version = match env::var("PYTAURI_PYTHON_VERSION") {
-        Ok(version) => version,
-        _ => "3.10.16".to_string()
-    };
-
-    let target_triple = env::var("TARGET")?;
-
-    let mut out_path = out_dir.clone();
-    out_path.push("pyembed");
-
-    download_standalone(&python_version, &target_triple, out_path.clone())?;
-
-    let location = out_path.canonicalize().unwrap().display().to_string();
-
-    let value = json!({
-        "bundle": {
-            "active": true,
-            "targets": "all",
-            "resources": {
-                location: "pyembed"
-            }
-        },
-    });
-
-    let json = value.to_string();
-
-    p!("json: {:?}", json);
-
-    println!("cargo:rustc-env=TAURI_CONFIG={}", json);
-    env::set_var("TAURI_CONFIG", json);
-
     tauri_build::build();
-
-    // panic!();
 
     Ok(())
 }
