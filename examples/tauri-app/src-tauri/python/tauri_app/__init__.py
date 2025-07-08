772aac8cc88f8f566f928d1077bdf619aec22ce6
WSH032 <614337162@qq.com>
2025-07-09 01:53:03 +0800

    feat: add `Manager::{state, try_state, manage}` and `async_tools`

--- examples/tauri-app/src-tauri/python/tauri_app/__init__.py
+++ examples/tauri-app/src-tauri/python/tauri_app/__init__.py
@@ -11,14 +11,14 @@ from datetime import datetime
 from functools import partial
 from pathlib import Path
 
-from anyio import create_task_group, sleep
-from anyio.abc import TaskGroup
+from anyio import sleep
 from anyio.from_thread import start_blocking_portal
 from pydantic import BaseModel, ConfigDict, RootModel
 from pydantic.alias_generators import to_camel
 from pytauri import (
     AppHandle,
     Commands,
+    Manager,
     builder_factory,
     context_factory,
 )
@@ -26,6 +26,7 @@ from pytauri.ipc import Channel, JavaScriptChannelId
 from pytauri.webview import WebviewWindow
 from pytauri_plugins.dialog import DialogExt, MessageDialogButtons, MessageDialogKind
 from pytauri_plugins.notification import NotificationExt
+from pytauri_utils.async_tools import AsyncTools
 
 from tauri_app.private import private_algorithm
 
@@ -60,24 +61,8 @@ async def start_timer(
     """
     time_channel = body.channel_on(webview_window.as_ref_webview())
 
-    # NOTE:
-    #
-    # - When this command (`start_timer`) is called, the `task_group` has already been created,
-    #   so we can use it directly.
-    #
-    # - The async context in which this command is called and the async context of the `task_group`
-    #   are both the same `blocking_portal`, so we don't need to worry about thread safety issues.
-    #   I.e, we can call `task_group.start_soon` directly, instead of needing use `portal.start_soon`.
-    #
-    #   Thread safety references:
-    #   - <https://anyio.readthedocs.io/en/stable/threads.html>
-    #   - <https://docs.python.org/3/library/asyncio-task.html#asyncio.run_coroutine_threadsafe>
-    #
-    # ---
-    #
-    # Or if you use `asyncio`, you can use `asyncio.create_task` derectly,
-    # so that you don't need init `task_group`.
-    task_group.start_soon(timer_task, time_channel)
+    async_tools = Manager.state(webview_window, AsyncTools)
+    async_tools.task_group.start_soon(timer_task, time_channel)
 
 
 class _BaseModel(BaseModel):
@@ -120,22 +105,15 @@ async def greet(
     return f"Hello, {body.name}! You've been greeted from Python {sys.version}!"
 
 
-# Anyio `TaskGroup` can only be created in async context,
-# So we need to use `portal.call` to create it,
-# and use `portal.wrap_async_context_manager` to enter it.
-task_group: TaskGroup
-
-
 def main() -> int:
     """Run the tauri-app."""
 
     # test if the code protected by Cython is working correctly
     assert private_algorithm(42) == 84, "private_algorithm is not working!"
 
-    global task_group
     with (
         start_blocking_portal("asyncio") as portal,  # or `trio`
-        portal.wrap_async_context_manager(portal.call(create_task_group)) as task_group,
+        AsyncTools(portal) as async_tools,
     ):
         if PYTAURI_GEN_TS:
             output_dir = Path(__file__).parent.parent.parent.parent / "src" / "client"
@@ -152,5 +130,7 @@ def main() -> int:
             context=context_factory(),
             invoke_handler=commands.generate_handler(portal),
         )
+        Manager.manage(app, async_tools)
+
         exit_code = app.run_return()
         return exit_code
