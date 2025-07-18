4d08dcf37da11c584d2925de958b50cebc466cb7
Sean Wang <126865849+WSH032@users.noreply.github.com>
2025-07-18 19:44:17 +0800

    wip

---
+++ foo copy.py
@@ -0,0 +1,74 @@
+from typing import Any, Awaitable, Callable, Union, cast
+
+from anyio import (
+    TASK_STATUS_IGNORED,
+    Event,
+    connect_tcp,
+    create_memory_object_stream,
+    create_task_group,
+    create_tcp_listener,
+    run,
+)
+from anyio.abc import TaskStatus
+from pytauri_utils.async_tools import AsyncTools
+from typing_extensions import (
+    Concatenate,
+    ParamSpec,
+    Protocol,
+    TypeVar,
+    TypeVarTuple,
+    Unpack,
+)
+
+T = TypeVar("T", infer_variance=True)
+T1 = TypeVar("T1", infer_variance=True)
+Ts = TypeVarTuple("Ts")
+P = ParamSpec("P")
+P1 = ParamSpec("P1")
+
+
+Exception_T = TypeVar(
+    "Exception_T", bound=BaseException, infer_variance=True, default=BaseException
+)
+
+Catch = Callable[[Exception_T], None]
+
+Then = Callable[[T], None]
+
+
+def chenge(
+    func: Callable[[Unpack[Ts], Then[T], Catch], None],
+) -> Callable[[Unpack[Ts]], Awaitable[T]]:
+    async def inner(*args: Unpack[Ts]) -> T:
+        send, recv = create_memory_object_stream[Union[T, BaseException]](
+            max_buffer_size=0
+        )
+
+        @async_tools.to_sync
+        async def then(ret: T) -> None:
+            async with send:
+                send.send_nowait(ret)
+
+        @async_tools.to_sync
+        async def catch(e: BaseException) -> None:
+            async with send:
+                send.send_nowait(e)
+
+        func(*args, then, catch)
+
+        async with recv:
+            ret = await recv.receive()
+
+        if isinstance(ret, BaseException):
+            raise ret
+        return ret
+
+    return inner
+
+
+def foo(
+    a: int, then: Callable[[str], None], catch: Callable[[Exception], None]
+) -> None: ...
+
+
+foo1_0 = chenge(foo)
