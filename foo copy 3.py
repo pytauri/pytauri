7c64fa932f33fbc6ffaf254a27d763f8ca88ee99
Sean Wang <126865849+WSH032@users.noreply.github.com>
2025-07-22 21:46:53 +0800

    wip

--- foo copy 3.py
+++ foo copy 3.py
@@ -140,58 +140,6 @@ class TaskGroupPoolExecutor(AbstractAsyncContextManager["TaskGroupPoolExecutor"]
             self._outer_task_group.start_soon(exit_stack.aclose)
 
 
-class FutureNursery(AbstractAsyncContextManager["FutureNursery"]):
-    def __init__(self) -> None:
-        self._exit_stack = AsyncExitStack()
-        self._portal = BlockingPortal()
-        self._task_group: TaskGroup = create_task_group()
-        self._event_loop_ident = get_ident()
-
-    async def __aenter__(self) -> Self:
-        await self._exit_stack.enter_async_context(self._portal)
-        await self._exit_stack.enter_async_context(self._task_group)
-        return self
-
-    async def __aexit__(self, *args: Any) -> Optional[bool]:
-        return await self._exit_stack.__aexit__(*args)
-
-    async def wait(self, future: futures.Future[T]) -> T:
-        sender, receiver = create_memory_object_stream[Union[T, BaseException]](1)
-
-        def _callback(future: futures.Future[T]) -> None:
-            with CancelScope(shield=True), sender:  # noqa: ASYNC100
-                if future.cancelled():
-                    sender.send_nowait(futures.CancelledError())
-                    return
-
-                exception = future.exception()
-                if exception is not None:
-                    sender.send_nowait(exception)
-                    return
-
-                ret = future.result()
-                sender.send_nowait(ret)
-
-        def callback(future: futures.Future[T]) -> None:
-            if get_ident() == self._event_loop_ident:
-                _callback(future)
-            else:
-                # TODO: HOW to ensure `acallback` will not be cancelled in any case?
-                self._portal.start_task_soon(_callback, future).add_done_callback(
-                    # NOTE: log
-                    lambda f: f.result(0)
-                )
-
-        future.add_done_callback(callback)
-
-        # TODO: cancelled exception handling
-        with receiver:
-            ret = await receiver.receive()
-            if isinstance(ret, BaseException):
-                raise ret
-            return ret
-
-
 # def future_to_coroutine(
 #     future: futures.Future[T],
 #     executor: Union[BlockingPortal, TaskGroup],
@@ -237,7 +185,7 @@ class FutureNursery(AbstractAsyncContextManager["FutureNursery"]):
 async def future_to_coroutine(
     future: futures.Future[T],
     executor: Union[BlockingPortal, TaskGroup],
-) -> T:
+) -> Coroutine[None, None, T]:
     event_loop_ident = get_ident()
     sender, receiver = create_memory_object_stream[Union[T, BaseException]](1)
 
@@ -255,29 +203,32 @@ async def future_to_coroutine(
             ret = future.result()
             sender.send_nowait(ret)
 
-    async def _async_callback(future: futures.Future[T]) -> None:
+    async def _acallback(future: futures.Future[T]) -> None:
         _callback(future)
 
     def callback(future: futures.Future[T]) -> None:
         if get_ident() == event_loop_ident:
             _callback(future)
         else:
+            # TODO: HOW to ensure `acallback` will not be cancelled in any case?
             if isinstance(executor, BlockingPortal):
-                # TODO: HOW to ensure `acallback` will not be cancelled in any case?
                 executor.start_task_soon(_callback, future).add_done_callback(
                     # NOTE: log
                     lambda f: f.result(0)
                 )
             else:
-                executor.start_soon(_async_callback, future)
+                executor.start_soon(_acallback, future)
 
     future.add_done_callback(callback)
 
-    with receiver:
-        ret = await receiver.receive()
-        if isinstance(ret, BaseException):
-            raise ret
-        return ret
+    async def wait_for_future() -> T:
+        with receiver:
+            ret = await receiver.receive()
+            if isinstance(ret, BaseException):
+                raise ret
+            return ret
+
+    return wait_for_future()
 
 
 if __name__ == "__main__":
@@ -292,7 +243,7 @@ if __name__ == "__main__":
         return 42
 
     with (
-        start_blocking_portal(backend="trio") as portal,
+        start_blocking_portal() as portal,
         BlockingPortalPoolExecutor(portal) as portal_executor,
     ):
         future = portal_executor.submit(foo)
@@ -302,16 +253,15 @@ if __name__ == "__main__":
                 create_task_group() as tg,
                 TaskGroupPoolExecutor(tg) as executor,
                 BlockingPortal() as blocking_portal,
-                FutureNursery() as future_nursery,
             ):
                 future1 = await executor.submit(foo)
-                result = await future_nursery.wait(future1)
+                result = await(await future_to_coroutine(future1, tg))
                 print(result)
 
-                result = await future_nursery.wait(future)
+                result = await(await future_to_coroutine(future, blocking_portal))
                 print("Result from future:", result)
 
-        run(main, backend="trio")
+        run(main)
 
     # from anyio import sleep
 
